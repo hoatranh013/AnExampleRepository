c7271cd8a4f3069e86d37544f5c18604bca3056c
hoatranh013 <106223595+hoatranh013@users.noreply.github.com>
2024-12-15 21:28:31 +0700

    Add files via upload

---
+++ WebApplicationInsertedDatabase/WebApplicationInsertedDatabase/Program.cs
@@ -0,0 +1,39 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using Npgsql;
+using System;
+using System.Data;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string connectionString = "Host=localhost;Username=your_username;Password=your_password;Database=your_database";
+        string getQuery = "SELECT id, version FROM version";
+        string insertQuery = "INSERT INTO version (id, version) VALUES (@value1, @value2)";
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            var getReadVersion = 0;
+            using (var getPreviousVersion = new NpgsqlCommand(getQuery, connection))
+            {
+                var readDatas = getPreviousVersion.ExecuteReader();
+                while (readDatas.Read())
+                {
+                    if (int.Parse(readDatas.GetValue(1).ToString()) > getReadVersion)
+                    {
+                        getReadVersion = int.Parse(readDatas.GetValue(1).ToString());
+                    }
+                }
+            }
+            getReadVersion = getReadVersion + 1;
+            using (var insertCommand = new NpgsqlCommand(insertQuery, connection))
+            {
+                insertCommand.Parameters.AddWithValue("@value1", Guid.NewGuid());
+                insertCommand.Parameters.AddWithValue("@value2", getReadVersion);
+                insertCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
